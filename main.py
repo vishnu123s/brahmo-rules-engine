c7c25abc86ca65debba3b106e9e0ee23e116db13
Vishnu <soddalavishnu75@gmail.com>
2026-05-30 12:08:48 +0530

    Implement JWT authentication and password utilities
    Added JWT authentication and password hashing functions.

--- main.py
+++ main.py
@@ -4,10 +4,43 @@ from supabase import create_client
 from dotenv import load_dotenv
 import os
 
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 load_dotenv()
 
 app = FastAPI()
 
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+    
 class User(BaseModel):
     id: str
     org_id: str
